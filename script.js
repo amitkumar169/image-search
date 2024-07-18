ec6457a0a55aa50ea114883019cdd1e0de8f3ab7
amitkumar169 <austinlalkhani2000@gmail.com>
2024-07-18 15:23:09 +0530

    image search

---
+++ script.js
@@ -0,0 +1,69 @@
+const accessKey = "r4S0CIodixj8HbTBcqMHi9Wode3TVp2GxFVtTj5SLTc";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1) {
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+    if (page > 1) {
+        showMore.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    searchImages();
+});
+
+
+let mode = document.getElementById("icon");
+
+mode.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+   
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png"
+    }
+    
+    else{
+        icon.src = "images/moon.png";
+    }
+}
